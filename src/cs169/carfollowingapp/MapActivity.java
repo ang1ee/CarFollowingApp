eb9e26aff4bdba61f1857278e3498101376d975f
Angeline Lee <aglee92@gmail.com>
2014-03-08 14:00:01 -0800

    added plot method

--- src/cs169/carfollowingapp/MapActivity.java
+++ src/cs169/carfollowingapp/MapActivity.java
@@ -1,5 +1,7 @@
 package cs169.carfollowingapp;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
@@ -12,22 +14,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity {
 
+    private GoogleMap map;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         
-        GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        this.map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        this.map.setMyLocationEnabled(true);
         
         LatLng location = this.getLocation();
-        
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 13));
-        
-        map.addMarker(new MarkerOptions()
-            .title("Sydney")
-            .snippet("The most populous city in Australia")
-            .position(location));
+        ArrayList<LatLng> coords = new ArrayList<LatLng>();
+        coords.add(location);
+        this.plot(coords);
     }
 
     @Override
@@ -37,9 +37,21 @@ public class MapActivity extends FragmentActivity {
         return true;
     }
 
-    public LatLng getLocation() {
-        return new LatLng(-33.867, 151.206);
+    public void plot(ArrayList<LatLng> coords) {
+        if (coords.size() < 1) {
+            return;
+        }
         
+        // I don't know what 13 encodes yet... i'll make a class variable eventually
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(coords.get(0), 13));
+        
+        for (int i = 0; i < coords.size(); i++) {
+            map.addMarker(new MarkerOptions().position(coords.get(i)));
+        }
+    }
+    
+    public LatLng getLocation() {
+        return new LatLng(-33.867, 151.206);   
     }
     
 }
