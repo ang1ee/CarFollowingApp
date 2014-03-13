454e5ad2b9f02fc6e725e0f0849bfa5a1d7ef854
nikki7647 <davidminjia@gmail.com>
2014-03-12 23:42:05 -0700

    FollowActivity AsyncTask

--- src/cs169/carfollowingapp/FollowActivity.java
+++ src/cs169/carfollowingapp/FollowActivity.java
@@ -6,14 +6,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+
+
 public class FollowActivity extends MapActivity {
+	private String loginUrl = Constants.BASE_SERVER_URL + "api/login";
+	private String username;
+	private int errCode;
+	private ArrayList<LatLng> coords = new ArrayList<LatLng>();
 	protected static final int SUCCESS = 1;
     protected static final int NO_SUCH_USER = -1;
     protected static final int USER_NOT_BROADCASTING = -2;
@@ -27,22 +35,17 @@ public class FollowActivity extends MapActivity {
 
         if (Constants.DEBUG) {
             LatLng location = new LatLng(Double.valueOf(90), Double.valueOf(90));
-            ArrayList<LatLng> coords = new ArrayList<LatLng>();
+            
             coords.add(location);
             this.plot(coords);
             return;
         }
         
         Intent intent = getIntent();
-        String username = intent.getStringExtra(FrontPageActivity.U_KEY);
-        try {
-        	LatLng location = this.getLocation(username);
-        	ArrayList<LatLng> coords = new ArrayList<LatLng>();
-            coords.add(location);
-            this.plot(coords);
-        } catch (Exception e) {
-        	handleError(e.getMessage());
-        }
+        username = intent.getStringExtra(FrontPageActivity.U_KEY);
+
+        new HttpAsyncTask().execute(loginUrl);
+
     }
 
     @Override
@@ -52,12 +55,63 @@ public class FollowActivity extends MapActivity {
         return true;
     }
     
+    private class HttpAsyncTask extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+
+        		JSONObject postData = new JSONObject();
+        		try {
+					postData.put("username", username);
+					JSONObject obj = SimpleHTTPPOSTRequester.makeHTTPPOSTRequest(Constants.BASE_SERVER_URL + "api/follow", postData);
+					return obj;
+        		} catch (JSONException e) {
+					handleError(e.getMessage());
+				} catch (RuntimeException e) {
+					handleError(e.getMessage());
+				}
+        		return null;
+        	
+        }
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            //Toast.makeText(getBaseContext(),result, Toast.LENGTH_LONG).show();
+            JSONObject fin;
+            try {
+                fin = result;
+                errCode = fin.getInt("status code");
+                switch (errCode) { //Updates the message on the Log In page, depending on the database response.
+                    case SUCCESS:
+                    	coords.add(new LatLng(fin.getDouble("latitude"), fin.getDouble("longitude")));
+                        plot(coords);
+                        break;
+                    case NO_SUCH_USER:
+                        handleError("User does not exist.");
+                        break;
+                    case USER_NOT_BROADCASTING:
+                        handleError("User is not broadcasting.");
+                        break;
+                    default:
+                        handleError("Unknown errCode.");
+                        break;
+                }
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+                handleError(e.getMessage());
+            }
+        }
+    }
+    
+    
+    
+    
+    
     // TODO: make sure non-authenticated users can't get anyone's location
     /* 
      * username: who to obtain the location point from
      * returns the obtained latitude/longitude in the form of LatLng, or null otherwise
      * Code snipet from: http://stackoverflow.com/questions/11213594/how-to-make-http-get-request-in-android
      */
+    /*
     public LatLng getLocation(String username) throws Exception {
     	try { 
     		JSONObject postData = new JSONObject();
@@ -86,12 +140,13 @@ public class FollowActivity extends MapActivity {
 		}
 		return null; 
     }
-    
+    */
     public void stopFollowing(View view) {
 	    Intent intent = new Intent(this, FrontPageActivity.class);
 	    startActivity(intent);
 	    finish();
 	}
+
     /*
     private class DisplayCurrentLocation extends AsyncTask<GoogleMap, Integer, Long> {
         protected Long doInBackground(GoogleMap... maps) {
