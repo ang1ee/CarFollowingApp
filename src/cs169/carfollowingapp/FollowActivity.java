1d8fb781e978e970447516d6964887cdbd30b438
Trineth <ventesw@gmail.com>
2014-03-26 17:58:55 -0400

    Updated follow. Maybe works.

--- src/cs169/carfollowingapp/FollowActivity.java
+++ src/cs169/carfollowingapp/FollowActivity.java
@@ -1,6 +1,9 @@
 package cs169.carfollowingapp;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +25,8 @@ public class FollowActivity extends MapActivity {
 	private int errCode;
 	private ArrayList<LatLng> coords = new ArrayList<LatLng>();
 	private String username;
+    private Timer followTimer = new Timer();
+    private int frequency = 100000;
 	protected static final int SUCCESS = 1;
     protected static final int NO_SUCH_USER = -1;
     protected static final int USER_NOT_BROADCASTING = -2;
@@ -40,7 +45,18 @@ public class FollowActivity extends MapActivity {
         myPassword = intent.getStringExtra(Constants.MY_P_KEY);
         username = intent.getStringExtra(Constants.U_KEY);
 
-        new HttpAsyncTask().execute(loginUrl);
+        //new HttpAsyncTask().execute(loginUrl);
+
+        FollowTask follow = new FollowTask();
+//        public void schedule (TimerTask task, long delay, long period)
+//        Schedule a task for repeated fixed-delay execution after a specific delay.
+//
+//        Parameters
+//        task  the task to schedule.
+//        delay  amount of time in milliseconds before first execution.
+//        period  amount of time in milliseconds between subsequent executions.
+
+        followTimer.schedule(follow, 0, frequency);
     }
 
     @Override
@@ -49,7 +65,56 @@ public class FollowActivity extends MapActivity {
         getMenuInflater().inflate(R.menu.follow, menu);
         return true;
     }
-    
+
+    class FollowTask extends TimerTask {
+        public void run() {
+            JSONObject postData = new JSONObject();
+            String result;
+            try {
+                postData.put("username", username);
+                JSONObject obj = SimpleHTTPPOSTRequester.makeHTTPPOSTRequest(Constants.BASE_SERVER_URL + "api/follow", postData);
+                result = obj.toString();
+            } catch (JSONException e) {
+                result = "JSON_EXCEPTION";
+            } catch (RuntimeException e) {
+                result = "RUNTIME_EXCEPTION";
+            } catch (Exception e) {
+                result = "ERROR";
+            }
+
+            JSONObject fin;
+            try {
+                if (result == "JSON_EXCEPTION") {
+                    handleError("JSON Error");
+                } else if (result == "RUNTIME_EXCEPTION") {
+                    handleError("Connection Error");
+                } else if (result == "ERROR") {
+                    handleError("Error");
+                }
+                fin = new JSONObject(result);
+                errCode = fin.getInt("status code");
+                switch (errCode) { //Updates the message on the Log In page, depending on the database response.
+                    case SUCCESS:
+                        coords.add(new LatLng(fin.getDouble("latitude"), fin.getDouble("longitude")));
+                        plot(coords);
+                        break;
+                    case NO_SUCH_USER:
+                        handleError("User does not exist.");
+                        break;
+                    case USER_NOT_BROADCASTING:
+                        handleError("User is not broadcasting.");
+                        break;
+                    default:
+                        handleError("Unknown errCode.");
+                        break;
+                }
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+                handleError(e.getMessage());
+            }
+        }
+    }
+
     private class HttpAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
@@ -57,7 +122,7 @@ public class FollowActivity extends MapActivity {
         		JSONObject postData = new JSONObject();
         		try {
 					postData.put("username", username);
-					JSONObject obj = SimpleHTTPPOSTRequester.makeHTTPPOSTRequest(Constants.BASE_SERVER_URL + "api/follow", postData);
+					JSONObject obj = SimpleHTTPPOSTRequester.makeHTTPPOSTRequest(urls[0], postData);
 					return obj.toString();
         		} catch (JSONException e) {
         			return "JSON_EXCEPTION";
@@ -85,8 +150,6 @@ public class FollowActivity extends MapActivity {
                 errCode = fin.getInt("status code");
                 switch (errCode) { //Updates the message on the Log In page, depending on the database response.
                     case SUCCESS:
-                    	coords.add(new LatLng(fin.getDouble("latitude"), fin.getDouble("longitude")));
-                        plot(coords);
                         break;
                     case NO_SUCH_USER:
                         handleError("User does not exist.");
@@ -101,6 +164,15 @@ public class FollowActivity extends MapActivity {
             } catch (Exception e) {
                 Log.d("InputStream", e.getLocalizedMessage());
                 handleError(e.getMessage());
+            }
+
+            if (errCode == SUCCESS) {
+                Intent intent = new Intent(getApplicationContext(), FrontPageActivity.class);
+                intent.putExtra(Constants.MY_U_KEY, myUsername);
+                intent.putExtra(Constants.MY_P_KEY, myPassword);
+                followTimer.cancel();
+                startActivity(intent);
+                finish();
             }
         }
     }
@@ -146,11 +218,7 @@ public class FollowActivity extends MapActivity {
     }
     */
     public void stopFollowing(View view) {
-	    Intent intent = new Intent(this, FrontPageActivity.class);
-	    intent.putExtra(Constants.MY_U_KEY, myUsername);
-        intent.putExtra(Constants.MY_P_KEY, myPassword);
-	    startActivity(intent);
-	    finish();
+        new HttpAsyncTask().execute(Constants.BASE_SERVER_URL + "api/follow_cancellation");
 	}
 
     @Override
